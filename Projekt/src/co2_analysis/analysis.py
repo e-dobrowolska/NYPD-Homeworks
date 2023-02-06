a083f33ee1a0238ad5267e27a37a4cb99c66bdd3
e-dobrowolska <ed429245@students.mimuw.edu.pl>
2023-02-06 18:58:13 +0100

    years range functionality

--- Projekt/src/co2_analysis/analysis.py
+++ Projekt/src/co2_analysis/analysis.py
@@ -2,28 +2,41 @@ import pandas as pd
 import numpy as np
 
 
-def co2_ranking(merged_data):
+def co2_ranking(merged_data, start_year=0, end_year=2022):
     """
     Creates a ranking of countries and their CO2 emission (top 5 countries in each year)
     :param merged_data: a DataFrame with Countries, years, CO2 emissions, CO2 emission Per Capita (obtained
     using merge_data() function on DataFrames: api_pop (countries, years, population), api_gdp (countries, years, GDP),
     co2 (countries, years, CO2)
+    :param start_year: year, from which the ranking should begin
+    :param end_year: year, on which the ranking should end
     :return: a DataFrame with 5 countries with the highest CO2 emission and CO2 Per Capita emission in each year
     """
-    ranking = merged_data.sort_values(['Year', 'Per Capita'], ascending=[True, False]).groupby('Year').head(5)
-    return ranking[["Year", "Country", "Total", "Per Capita"]]
+    if(start_year<=end_year):
+        ranking = merged_data.sort_values(['Year', 'Per Capita'], ascending=[True, False]).groupby('Year').head(5)
+        return ranking[["Year", "Country", "Total", "Per Capita"]][(ranking["Year"] >= start_year) & (ranking["Year"] <= end_year)]
+    else:
+        print(r"Error: Start Year can't be after End Year.")
+        return
 
 
-def gdp_ranking(merged_data):
+def gdp_ranking(merged_data, start_year=0, end_year=2022):
     """
     Creates a ranking of countries and their GDP (top 5 countries in each year)
     :param merged_data: a DataFrame with countries, years, GDP (obtained using merge_data() function on DataFrames:
     api_pop (countries, years, population), api_gdp (countries, years, GDP), co2 (countries, years, CO2)
+    :param start_year: year, from which the ranking should begin
+    :param end_year: year, on which the ranking should end
     :return: a DataFrame with 5 countries with the highest GDP and GDP Per Capita in each year
     """
-    merged_data["GDP Per Capita"] = merged_data["GDP"]/merged_data["Population"]
-    ranking = merged_data.sort_values(['Year', 'GDP Per Capita'], ascending=[True, False]).groupby('Year').head(5)
-    return ranking[["Year", "Country", "GDP", "GDP Per Capita"]]
+    if(start_year<=end_year):
+        merged_data["GDP Per Capita"] = merged_data["GDP"]/merged_data["Population"]
+        ranking = merged_data.sort_values(['Year', 'GDP Per Capita'], ascending=[True, False]).groupby('Year').head(5)
+        return ranking[["Year", "Country", "GDP", "GDP Per Capita"]][(ranking["Year"] >= start_year) & (ranking["Year"] <= end_year)]
+    else:
+        print(r"Error: Start Year can't be after End Year.")
+        return
+
 
 
 def co2_reduction_ranking(merged_data, end_year, number_of_years=10):
@@ -40,4 +53,4 @@ def co2_reduction_ranking(merged_data, end_year, number_of_years=10):
     pivot_df["Reduction"] = pivot_df[end_year]-pivot_df[end_year-number_of_years]
     pivot_df = pivot_df["Reduction"].sort_values()
     pivot_df = pivot_df.dropna()
-    return pivot_df.head(5), pivot_df.tail(5).sort_values(ascending=False)
+    return pd.DataFrame(pivot_df.head(5)), pd.DataFrame(pivot_df.tail(5).sort_values(ascending=False))
